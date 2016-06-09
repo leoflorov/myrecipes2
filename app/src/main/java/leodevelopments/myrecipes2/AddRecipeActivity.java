5891d98ee7384376ba72e7c570d7e24b645ff4ac
leoflorov <leoflorov@mail.ru>
2016-06-09 23:35:20 +0300

    Start

---
+++ app/src/main/java/leodevelopments/myrecipes2/AddRecipeActivity.java
@@ -0,0 +1,47 @@
+package leodevelopments.myrecipes2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+public class AddRecipeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.addrecipe_layout);
+
+
+    String[] categoriesAutoComplete = {"Салаты", "Супы", "Вторые блюда", "Напитки", "Десерты", "Курица", "Рыба", "Мясо", "Праздники"};
+
+        AutoCompleteTextView textView = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, categoriesAutoComplete);
+        textView.setAdapter(adapter);
+
+
+
+
+//
+//            // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
+//            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categoriesSpinner);
+//            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//
+//            Spinner spinner = (Spinner) findViewById(R.id.spinner);
+//            spinner.setAdapter(adapter);
+//        spinner.setPrompt("Категории рецептов");
+//        spinner.setSelection(2);
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Toast.makeText(getBaseContext(), "Position =" + position, Toast.LENGTH_SHORT).show();
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+
+        }
+    }
