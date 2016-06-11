96818e1135669a016e74c12fac66666f0bb97e26
leoflorov <leoflorov@mail.ru>
2016-06-11 23:54:56 +0300

    SQLite 2

--- app/src/main/java/leodevelopments/myrecipes2/AddRecipeActivity.java
+++ app/src/main/java/leodevelopments/myrecipes2/AddRecipeActivity.java
@@ -1,34 +1,50 @@
 package leodevelopments.myrecipes2;
 
+import android.app.Activity;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class AddRecipeActivity extends AppCompatActivity {
+public class AddRecipeActivity extends Activity implements View.OnClickListener {
 
     private DatabaseHelper mDataBaseHelper;
     private SQLiteDatabase mSQLiteDatabase;
+    Button btnAdd;
+    EditText etName, etIngredients, etInstructions, etPhoto;
+    DatabaseHelper dbHelper;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.addrecipe_layout);
 
+        btnAdd = (Button) findViewById(R.id.saveRecipeButton);
+        btnAdd.setOnClickListener(this);
 
-        mDataBaseHelper = new DatabaseHelper(this, "myrecipes.db", null, 1);
+        etName = (EditText) findViewById(R.id.recipe_name);
+        etIngredients = (EditText) findViewById(R.id.add_ingredients_txt);
+        etInstructions = (EditText) findViewById(R.id.add_instructions_txt);
 
-        mSQLiteDatabase = mDataBaseHelper.getWritableDatabase();
+        dbHelper = new DatabaseHelper(this);
 
-        ContentValues values = new ContentValues();
-        //задание значения для каждого столбца
-        values.put(DatabaseHelper.RECIPE_NAME_COLUMN, "Оивье");
-        values.put(DatabaseHelper.PHOTO_COLUMN, "@drawable/salad.jpg");
-        values.put(DatabaseHelper.INGREDIENTS_COLUMN, "Колбаса, майонез, яйца и т.д.");
-        values.put(DatabaseHelper.INSTRUCTION_COLUMN, "ПРиготовить салат");
+
+//        mDataBaseHelper = new DatabaseHelper(this, "myrecipes.db", null, 1);
+//
+//        mSQLiteDatabase = mDataBaseHelper.getWritableDatabase();
+//
+//        ContentValues values = new ContentValues();
+//        //задание значения для каждого столбца
+//        values.put(DatabaseHelper.RECIPE_NAME_COLUMN, "Оивье");
+//        values.put(DatabaseHelper.PHOTO_COLUMN, "@drawable/salad.jpg");
+//        values.put(DatabaseHelper.INGREDIENTS_COLUMN, "Колбаса, майонез, яйца и т.д.");
+//        values.put(DatabaseHelper.INSTRUCTION_COLUMN, "ПРиготовить салат");
 
         String[] categoriesAutoComplete = {"Салаты", "Супы", "Вторые блюда", "Напитки", "Десерты", "Курица", "Рыба", "Мясо", "Праздники"};
 
@@ -36,6 +52,7 @@ public class AddRecipeActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, categoriesAutoComplete);
         textView.setAdapter(adapter);
 
+    }
 //            // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
 //            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categoriesSpinner);
 //            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -49,18 +66,60 @@ public class AddRecipeActivity extends AppCompatActivity {
 //            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 //                Toast.makeText(getBaseContext(), "Position =" + position, Toast.LENGTH_SHORT).show();
 //            }
-//
-//            @Override
-//            public void onNothingSelected(AdapterView<?> parent) {
-//
-//            }
-//        });
 
-    }
-
+    @Override
     public void onClick(View v) {
-        mDataBaseHelper = new DatabaseHelper(this, "myrecioes,db", null, 1);
-        SQLiteDatabase sdb;
-        sdb = mDataBaseHelper.getReadableDatabase();
+
+        String name = etName.getText().toString();
+        String ingredients = etIngredients.getText().toString();
+        String instructions = etInstructions.getText().toString();
+
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        ContentValues contentValues = new ContentValues();
+
+
+        switch (v.getId()) {
+
+            case R.id.saveRecipeButton:
+                contentValues.put(DatabaseHelper.RECIPE_NAME_COLUMN, name);
+                contentValues.put(DatabaseHelper.INGREDIENTS_COLUMN, ingredients);
+                contentValues.put(DatabaseHelper.INSTRUCTION_COLUMN, instructions);
+
+                database.insert(DatabaseHelper.DATABASE_TABLE, null, contentValues);
+                break;
+
+            case R.id.btnRead:
+                Cursor cursor = database.query(DatabaseHelper.DATABASE_TABLE, null, null, null, null, null, null);
+
+                if (cursor.moveToFirst()) {
+                    int idIndex = cursor.getColumnIndex(DatabaseHelper._ID);
+                    int nameIndex = cursor.getColumnIndex(DatabaseHelper.RECIPE_NAME_COLUMN);
+                    int ingredientsIndex = cursor.getColumnIndex(DatabaseHelper.INGREDIENTS_COLUMN);
+                    int instructionsIndex = cursor.getColumnIndex(DatabaseHelper.INSTRUCTION_COLUMN);
+                    do {
+                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
+                                ", name = " + cursor.getString(nameIndex) +
+                                ", ingredients = " + cursor.getString(ingredientsIndex) +
+                                ", instructions = " + cursor.getString(instructionsIndex));
+                    } while (cursor.moveToNext());
+                } else
+                    Log.d("mLog", "0 rows");
+
+                cursor.close();
+                break;
+
+            case R.id.btnClear:
+                database.delete(DatabaseHelper.DATABASE_TABLE, null, null);
+                break;
+        }
+        dbHelper.close();
     }
+//    public void onClick(View v) {
+//        mDataBaseHelper = new DatabaseHelper(this, "myrecioes,db", null, 1);
+//        SQLiteDatabase sdb;
+//        sdb = mDataBaseHelper.getReadableDatabase();
 }
+
+
--- app/src/main/java/leodevelopments/myrecipes2/DatabaseHelper.java
+++ app/src/main/java/leodevelopments/myrecipes2/DatabaseHelper.java
@@ -13,44 +13,52 @@ public class DatabaseHelper extends SQLiteOpenHelper implements BaseColumns {
     //версия базы данных
     private static final int DATABASE_VERSION = 1;
     //имя таблицы
-    private static final String DATABASE_TABLE = "recipes";
+    public static final String DATABASE_TABLE = "recipes";
     //названия столбцов
     public static final String RECIPE_NAME_COLUMN = "recipe_name";
     public static final String INSTRUCTION_COLUMN = "instruction";
     public static final String PHOTO_COLUMN = "photo";
     public static final String INGREDIENTS_COLUMN = "ingredients";
-    private static final String DATABASE_CREATE_SCRIPT = " create table "
-            + DATABASE_TABLE + " ("+ BaseColumns._ID
-            + " integer primary key autoincrement, " + RECIPE_NAME_COLUMN
-            + " text to null, " + PHOTO_COLUMN + " text to null, " + INGREDIENTS_COLUMN
-            + " text to null, " + INSTRUCTION_COLUMN + " text to null);";
+//    private static final String DATABASE_CREATE_SCRIPT = " create table "
+//            + DATABASE_TABLE + " ("+ BaseColumns._ID
+//            + " integer primary key autoincrement, " + RECIPE_NAME_COLUMN
+//            + " text to null, " + PHOTO_COLUMN + " text to null, " + INGREDIENTS_COLUMN
+//            + " text to null, " + INSTRUCTION_COLUMN + " text to null);";
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public DatabaseHelper (Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
-        super(context, name, factory, version, errorHandler);
-    }
+//    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+//        super(context, name, factory, version, errorHandler);
+//    }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(DATABASE_CREATE_SCRIPT);
+        db.execSQL("create table " + DATABASE_TABLE + "(" + _ID
+                + " integer primary key," + RECIPE_NAME_COLUMN + " text," + INGREDIENTS_COLUMN + " text" + ")");
 
     }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //запишем в журнал
-        Log.w("SQLite", "Обновляемся с версии " + oldVersion + " на версию " + newVersion);
+        db.execSQL("drop table if exists " + DATABASE_TABLE);
 
-        //удаляем старую таблицу и созадём новую
-        db.execSQL("DROP TABLE IF IT EXISTS " + DATABASE_TABLE);
-        // создаём новую таблицу
         onCreate(db);
-    }
 
-    DatabaseHelper(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 }
+
+//    @Override
+//    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+//        //запишем в журнал
+//        Log.w("SQLite", "Обновляемся с версии " + oldVersion + " на версию " + newVersion);
+//
+//        //удаляем старую таблицу и созадём новую
+//        db.execSQL("DROP TABLE IF IT EXISTS " + DATABASE_TABLE);
+//        // создаём новую таблицу
+//        onCreate(db);
+//    }
+//
+//    DatabaseHelper(Context context) {
+//        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+//    }
